f7178fb21a674d2fd6b17b0189873fdbf108bde2
ashburn noronha <109790462+ashburn-nrh@users.noreply.github.com>
2025-03-17 14:44:37 +0530

    mailbox

--- vite-project/src/pages/email.jsx
+++ vite-project/src/pages/email.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const Email = () => {
   return (
     <>
     <div className="grid grid-cols-2 items-center bg-white shadow p-6 h-16">
     <h2 className="flex justify-start font-medium text-xl">Emails</h2>
-    </div></>
+    </div>
+    <div className="flex justify-center items-center h-screen">
+  <Button className="bg-blue-500 text-white px-4 py-2 rounded-lg">Add a mailbox</Button>
+</div>
+    </>
   )
 }
 
